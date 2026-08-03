7a88dc5ad95050982598ea2d38e85263075016b0
ritesh chaudhary <chudharyritesh2078@gmail.com>
2026-08-03 07:57:58 +0545

    added new

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,7 +4,7 @@ export default async function connectDB() {
   const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/banking_db';
   try {
     await mongoose.connect(uri);
-    console.log('MongoDB connected:', uri);
+    console.log('MongoDB connected:', mongoose.connection.host);
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
     process.exit(1);
